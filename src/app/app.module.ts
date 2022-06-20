4793f0d207e23f775a90cdebcda3c8765e10f962
Samuel-Brr <samuel.barre972@gmail.com>
2022-06-20 12:20:32 +0200

    Ajout des principaux composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateKittenComponent } from './create-kitten/create-kitten.component';
+import { ListKittenComponent } from './list-kitten/list-kitten.component';
+import { UserKittenComponent } from './user-kitten/user-kitten.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateKittenComponent,
+    ListKittenComponent,
+    UserKittenComponent
   ],
   imports: [
     BrowserModule,
